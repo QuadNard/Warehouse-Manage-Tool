831f205f7531c14e28c2b93decab0417850b3a88
QuardNard <tullochyorkcity@gmail.com>
2022-08-19 03:00:13 -0400

    Setup frontend functionality

--- warehouse_app/components/form.js
+++ warehouse_app/components/form.js
@@ -1,63 +1,14 @@
-import { useReducer } from "react"
-
-
-const formReducer = (state, event) => {
-return {
-    ...state,
-    [event.target.name]: event.target.value
-}
-}
+import AddUserForm from "./addUserForm";
+import UpdateUserForm from "./updateUserForm";
 
 export default function Form(){
 
-      const [formData, setFormData] =useReducer(formReducer, {})
+ const flag = true;
 
-      const handleSubmit = (e) => {
-        e.preventDefault(); 
-        console.log(formData)
-      }
-    
-    return(
-        <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
-            <div className="input-type">
-                <input type="text" onChange={setFormData} name="id" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="ID" />
-            </div>
-
-            <div className="input-type">
-            <input type="text" onChange={setFormData} name="name" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Name" />
-            </div>
-
-            <div className="input-type">
-            <input type="text" onChange={setFormData} name="weight" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Weight" />
-            </div>
-
-            <div className="input-type">
-            <input type="text" onChange={setFormData} name="case stock" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Case Stock" />
-            </div>
-             
-
-
-
-    <div className="flex gap-10 items-center">
-            <div className="form-check">
-            <input type="radio" onChange={setFormData}  value="Active Unloading" id="radioDefault1" name="status" className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"></input>
-            <label htmlFor="radioDefault1" className="inline-block text-gray-800">
-                Active Unloading
-            </label>
-            <div className="form-check">
-            <input type="radio" onChange={setFormData} value="Halted load" id="radioDefault2" name="status" className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-red-500 checked:border-red-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"></input>
-            <label htmlFor="radioDefault2" className="inline-block text-gray-800">
-                 Halted load
-            </label>
-             </div>
-            </div>
-        </div>
-
-
-             <button className="flex-justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">Add
-             </button> 
-
-
-        </form>
-    )
+  return (
+    <div className="container mx-auto py-5">
+          {flag? <AddUserForm/>:<UpdateUserForm/>}
+    </div>
+  
+  )
 }
--- warehouse_app/pages/index.js
+++ warehouse_app/pages/index.js
@@ -2,8 +2,16 @@ import Head from 'next/head'
 import {GiCardboardBoxClosed} from 'react-icons/gi'
 import Table from '../components/table'
 import Form from '../components/form'
+import { useState } from 'react'
 
 export default function Home() {
+
+const [visible, setVisible]  = useState(false)
+
+const handler = () => {
+  setVisible(!visible)
+}
+
   return (
     <section>
       <Head>
@@ -17,14 +25,14 @@ export default function Home() {
  
      <div className="container mx-auto flex justify-between py-5 border-b">
           <div className="left flex gap-3">
-            <button className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-g-800'>
+            <button onClick={handler} className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-g-800'>
               Add New Product  <span className='px-1'><GiCardboardBoxClosed size={23}></GiCardboardBoxClosed></span>
             </button>
           </div>
      </div>
 
      <div className="container mx-auto py-5">
-       <Form></Form>
+       { visible ? <Form></Form>:<></>}
      </div>
 
 
